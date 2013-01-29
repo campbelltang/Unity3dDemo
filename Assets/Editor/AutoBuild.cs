f4d8161e7841efddb59ae46b68e94df16072375e
candidate <candidate@its-MacBook.local>
2013-01-29 14:12:03 +0800

    change Editor settings

---
+++ Assets/Editor/AutoBuild.cs
@@ -0,0 +1,31 @@
+using UnityEditor;
+using System.Collections;
+using System.IO;
+using System;
+
+public class AutoBuild
+{
+
+  public static void PerformBuild() {
+    ArrayList sceneFiles = new ArrayList();
+
+    foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+      sceneFiles.Add(scene.path);
+    }
+
+    string[] scenes = (string[])sceneFiles.ToArray(typeof(string));
+
+    string outdir = "/Users/Shared/workspace/Unity3dDemo";
+
+    BuildPipeline.BuildPlayer(scenes, outdir, BuildTarget.iPhone, BuildOptions.None);
+
+    EditorApplication.Exit(0);
+  }
+
+  public static void PerformDemoBuild() {
+    PlayerSettings.productName = "Unity3dDemo";
+    PlayerSettings.bundleIdentifier = "com.happylatte.Unidy3dDemo";
+    EditorUserBuildSettings.development = true;
+    PerformBuild();
+  }
+}
